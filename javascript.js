fa2c9f46bc5846c5e59460481726806ba24d67b3
“Sam <scha4@gmu.edu>
2023-03-03 16:59:26 +0900

    added buttons

--- javascript.js
+++ javascript.js
@@ -1,35 +1,63 @@
+//UI
+let userScore = 0;
+let compScore = 0;
+
+const userScoreSpan = document.getElementById("userScore");
+const compScoreSpan = document.getElementById("compScore");
+const rockDiv = document.getElementById("r");
+const paperDiv = document.getElementById("p");
+const scissorsDiv = document.getElementById("s");
+const results = document.getElementById("result");
+
+//UI
+
+rockDiv.addEventListener("click", function () {
+  play("rock");
+});
+paperDiv.addEventListener("click", function () {
+  play("paper");
+});
+scissorsDiv.addEventListener("click", function () {
+  play("scissors");
+});
+
+// How the game functions
 const options = ["rock", "paper", "scissors"];
+const computerSelection = computerPicks();
 
 function computerPicks() {
   const choice = options[Math.floor(Math.random() * options.length)];
   return choice;
 }
-function checkWinner(playerSelection, computerSelection) {
+
+function play(playerSelection) {
   if (playerSelection == computerSelection) {
-    return "Tie";
+    results.innerHTML = `Its a tie`;
   } else if (
     (playerSelection == "rock" && computerSelection == "scissors") ||
     (playerSelection == "paper" && computerSelection == "rock") ||
     (playerSelection == "scissors" && computerSelection == "paper")
   ) {
-    return "Win";
+    userScore++;
+    userScoreSpan.innerHTML = userScore;
+    results.innerHTML = `${playerSelection} beats ${computerSelection}`;
   } else {
-    return "lose";
+    compScore++;
+    compScoreSpan.innerHTML = compScore;
+    results.innerHTML = `${computerSelection} beats ${playerSelection}`;
   }
 }
 
-function play(playerSelection, computerSelection) {
-  const result = checkWinner(playerSelection, computerSelection);
-  if (result == "Tie") {
-    return "it was a tie";
-  }
-  if (result == "Win") {
-    return "you have won";
-  }
-  if (result == "lose") {
-    return "you suck";
-  }
-}
-let playerSelection = "rock";
-const computerSelection = computerPicks();
-console.log(play(playerSelection, computerSelection));
+// function play(playerSelection, computerSelection) {
+//   const result = checkWinner(playerSelection, computerSelection);
+
+//   if (result == "Tie") {
+//     return "it was a tie";
+//   }
+//   if (result == "Win") {
+//     return "you have won";
+//   }
+//   if (result == "Lose") {
+//     return "you suck";
+//   }
+// }
